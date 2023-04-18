fda658734280fe029016d7c4af8b2c7f01093f7e
fzippy <hk2011khan@gmail.com>
2023-04-18 20:50:04 +0100

    Made login box

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,21 @@ function App() {
 
   return (
     <div className="App">
-      <p>Removed boiler Plate</p>
+      <div id="loginContainer">
+        <img src="src/assets/img/logo.png" id="loginLogo" />
+        <input type="text" className="inputField" placeholder="Username" />
+        <input type="text" className="inputField" placeholder="Password" />
+        <input type="submit" className="sbtn" style={{ marginTop: "10px" }} />
+        <div
+          className="row alignCenter"
+          style={{ width: "100%", margin: "10px 0" }}
+        >
+          <div className="lineSeperator"></div>
+          <p style={{ width: "max-content" }}>OR</p>
+          <div className="lineSeperator"></div>
+        </div>
+        <p>Sign in with Google</p>
+      </div>
     </div>
   );
 }
