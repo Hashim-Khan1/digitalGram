4b994722d76ac00d42f14d5d35621a08861470fc
fzippy <hk2011khan@gmail.com>
2023-04-19 22:05:48 +0100

    Made nav component

--- src/App.tsx
+++ src/App.tsx
@@ -3,31 +3,14 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import Login from "./components/Login";
+import Nav from "./components/Nav";
 function App() {
   const [count, setCount] = useState(0);
 
-  const navItems = ["Home", "Messages", "Profile"];
-
-  const renderNavItems = (navItems: any) => {
-    return navItems.map((el: string) => {
-      return (
-        <a>
-          <li key={el}>
-            <img src={`/src/assets/img/${el}.png`} className="navIcons" />
-            <p>{el}</p>
-          </li>
-        </a>
-      );
-    });
-  };
   return (
     <div className="App">
       {/* <Login></Login> */}
-
-      <nav>
-        <div className="navLogo"></div>
-        <ul>{renderNavItems(navItems)}</ul>
-      </nav>
+      <Nav />
     </div>
   );
 }
---
+++ src/components/Nav.tsx
@@ -0,0 +1,25 @@
+function Nav() {
+  const navItems = ["Home", "Messages", "Profile"];
+
+  const renderNavItems = (navItems: any) => {
+    return navItems.map((el: string) => {
+      return (
+        <a>
+          <li key={el}>
+            <img src={`/src/assets/img/${el}.png`} className="navIcons" />
+            <p>{el}</p>
+          </li>
+        </a>
+      );
+    });
+  };
+  return (
+    <>
+      <nav>
+        <div className="navLogo"></div>
+        <ul>{renderNavItems(navItems)}</ul>
+      </nav>
+    </>
+  );
+}
+export default Nav;
