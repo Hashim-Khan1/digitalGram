731fe58e17a75154622e5604b33c8b3bae082edb
fzippy <hk2011khan@gmail.com>
2023-04-20 19:44:23 +0100

    Created post container

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ function App() {
       {/* <Login></Login> */}
       <Nav />
       <div id="container">
-        <div className="postContainer">
+        <div className="mainBody">
           <div id="searchContainer">
             <input
               type="text"
@@ -20,6 +20,33 @@ function App() {
               placeholder="Search Username"
             />
             <div id="search"></div>
+          </div>
+          <div className="postContainer">
+            <div
+              className="row"
+              style={{ alignItems: "center", marginBottom: "10px" }}
+            >
+              <div className="profileImg"></div>
+              <p>USERNAME</p>
+              <p>7ds</p>
+            </div>
+            <div className="postImg"></div>
+            <div
+              className="row"
+              style={{ alignItems: "center", margin: "10px 0" }}
+            >
+              <div className="postIcons heart"></div>
+              <div className="postIcons comment"></div>
+              <div className="postIcons send"></div>
+            </div>
+            <p>111,000 likes</p>
+            <p>
+              {" "}
+              <b>Uername</b> Lorem ipsum dolor sit amet consectetur adipisicing
+              elit. Perspiciatis autem quis dolore molestiae, minima quia
+              veritatis impedit facilis! Neque, eos.{" "}
+            </p>
+            <p className="greyText">View all comments 115 </p>
           </div>
         </div>
       </div>
