89ed1709af49a91829c3a4c7f00c4c529bf6bf96
fzippy <hk2011khan@gmail.com>
2023-04-24 21:00:09 +0100

    Finished template for messages

--- src/App.tsx
+++ src/App.tsx
@@ -35,12 +35,39 @@ function App() {
               <p>Username</p>
             </div>
           </div>
-          <div id="msgHeader" className="row alignCenter">
-            <p style={{ fontSize: "18px" }}>Username</p>
-            <div
-              className="navIcons phone"
-              style={{ marginLeft: "auto" }}
-            ></div>
+          <div id="msgBody" style={{ width: "100%" }}>
+            <div id="msgHeader" className="row alignCenter">
+              <p style={{ fontSize: "18px" }}>Username</p>
+              <div
+                className="navIcons phone"
+                style={{ marginLeft: "auto" }}
+              ></div>
+            </div>
+            <div id="userMessages">
+              <div className="userMsg myMsg">
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                Numquam tempora a, ad aspernatur officiis facilis.
+              </div>
+              <div className="userMsg toMsg">Lorem ipsum dolor sit amet.</div>
+            </div>
+
+            <div className="row">
+              <input
+                type="text"
+                className="inputField"
+                style={{ color: "black" }}
+              />
+              <div
+                style={{
+                  marginLeft: "auto",
+                  border: "1px solid rgba(128, 128, 128, 0.123) ",
+                  padding: "5px 12px",
+                  cursor: "pointer",
+                }}
+              >
+                SEND
+              </div>
+            </div>
           </div>
         </div>
       </div>
