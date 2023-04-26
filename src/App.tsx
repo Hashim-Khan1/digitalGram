fd7cf6d45a00cc4439d9b9f92dc60029864bf7c9
fzippy <hk2011khan@gmail.com>
2023-04-27 00:35:21 +0100

    Posts for users

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import "./App.css";
 import Login from "./components/Login";
 import Nav from "./components/Nav";
 import Posts from "./components/Posts";
+import Users from "./components/Users";
 function App() {
   const [count, setCount] = useState(0);
 
@@ -26,14 +27,56 @@ function App() {
         </div>
         <div
           id="messageContainer"
+          style={{
+            border: "1px solid rgba(128, 128, 128, 0.123)",
+            padding: "10px",
+          }}
+        >
+          <div id="profileHeader" className="row">
+            <div id="profileLarge"></div>
+
+            <div>
+              <div id="profileControls">
+                <p style={{ fontSize: "25px" }}>Username</p>
+                <div className="row">
+                  <div className="profileControlsBtn">Following</div>
+                  <div className="profileControlsBtn">Messages</div>
+                </div>
+              </div>
+              <div id="userInfoBox">
+                <p>Name</p>
+                <p>
+                  Bio Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  In, amet?
+                </p>
+              </div>
+            </div>
+          </div>
+          <hr
+            style={{
+              backgroundColor: "rgba(128, 128, 128, 0.123)",
+              height: "1px",
+              border: "0",
+              margin: "25px 0 10px 0",
+            }}
+          />
+          <p style={{ textAlign: "center" }}>POSTS</p>
+          <div id="postsContentContainer">
+            <div className="posts"></div>
+            <div className="posts"></div>
+            <div className="posts"></div>
+            <div className="posts"></div>
+          </div>
+        </div>
+        {/* 
+        Inbox Page
+        <div
+          id="messageContainer"
           className="row"
           style={{ border: "1px solid rgba(128, 128, 128, 0.123)" }}
         >
           <div id="userMsgContainer">
-            <div className="userBox row alignCenter">
-              <div className="userProfile"></div>
-              <p>Username</p>
-            </div>
+            <Users />
           </div>
           <div id="msgBody" style={{ width: "100%" }}>
             <div id="msgHeader" className="row alignCenter">
@@ -69,7 +112,7 @@ function App() {
               </div>
             </div>
           </div>
-        </div>
+        </div> */}
       </div>
     </div>
   );
