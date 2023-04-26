9db12c47550a8943e6335dfab4c8164a53e4e6c1
fzippy <hk2011khan@gmail.com>
2023-04-27 00:35:31 +0100

    Split users card into component

---
+++ src/components/Users.tsx
@@ -0,0 +1,11 @@
+function Users() {
+  return (
+    <>
+      <div className="userBox row alignCenter">
+        <div className="userProfile"></div>
+        <p>Username</p>
+      </div>
+    </>
+  );
+}
+export default Users;
