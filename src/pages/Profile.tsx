567c694c7ad4e1ec3fbd43c2caa2c4aca72e5f52
fzippy <hk2011khan@gmail.com>
2023-04-27 22:34:35 +0100

    Minor css bug fix

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -4,45 +4,46 @@ function Profile() {
   return (
     <>
       <Nav />
+      <div id="container">
+        <div
+          id="messageContainer"
+          style={{
+            border: "1px solid rgba(128, 128, 128, 0.123)",
+            padding: "10px",
+          }}
+        >
+          <div id="profileHeader" className="row">
+            <div id="profileLarge"></div>
 
-      <div
-        id="messageContainer"
-        style={{
-          border: "1px solid rgba(128, 128, 128, 0.123)",
-          padding: "10px",
-        }}
-      >
-        <div id="profileHeader" className="row">
-          <div id="profileLarge"></div>
-
-          <div>
-            <div id="profileControls">
-              <p style={{ fontSize: "25px" }}>Username</p>
-              <div className="row">
-                <div className="profileControlsBtn">Following</div>
-                <div className="profileControlsBtn">Messages</div>
+            <div>
+              <div id="profileControls">
+                <p style={{ fontSize: "25px" }}>Username</p>
+                <div className="row">
+                  <div className="profileControlsBtn">Following</div>
+                  <div className="profileControlsBtn">Messages</div>
+                </div>
+              </div>
+              <div id="userInfoBox">
+                <p>Name</p>
+                <p>
+                  Bio Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  In, amet?
+                </p>
               </div>
             </div>
-            <div id="userInfoBox">
-              <p>Name</p>
-              <p>
-                Bio Lorem ipsum dolor sit amet consectetur adipisicing elit. In,
-                amet?
-              </p>
-            </div>
           </div>
-        </div>
-        <hr
-          style={{
-            backgroundColor: "rgba(128, 128, 128, 0.123)",
-            height: "1px",
-            border: "0",
-            margin: "25px 0 10px 0",
-          }}
-        />
-        <p style={{ textAlign: "center" }}>POSTS</p>
-        <div id="postsContentContainer">
-          <div className="posts"></div>
+          <hr
+            style={{
+              backgroundColor: "rgba(128, 128, 128, 0.123)",
+              height: "1px",
+              border: "0",
+              margin: "25px 0 10px 0",
+            }}
+          />
+          <p style={{ textAlign: "center" }}>POSTS</p>
+          <div id="postsContentContainer">
+            <div className="posts"></div>
+          </div>
         </div>
       </div>
     </>
