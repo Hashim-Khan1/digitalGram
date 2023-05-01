83e45cc82295c47f7eae787d1d6f3022dcf1cfc3
fzippy <hk2011khan@gmail.com>
2023-05-02 00:33:57 +0100

    Added Cors

--- server/index.js
+++ server/index.js
@@ -1,8 +1,14 @@
 const app = require("express")();
 const bodyParser = require("body-parser");
+const cors = require("cors");
 const urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 const routes = require("./api/index");
+const corsOptions = {
+  origin: "http://localhost:5173",
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+app.use(cors(corsOptions));
 routes(app, { urlencodedParser });
 
 const PORT = 3000;
