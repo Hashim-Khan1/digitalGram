8645e0ba7a6e49972afc8251468d34021a79a0f4
fzippy <hk2011khan@gmail.com>
2023-05-08 23:53:22 +0100

    Called function nd added the parameters n sent res

--- server/api/post.js
+++ server/api/post.js
@@ -2,7 +2,8 @@ const express = require("express");
 const router = express.Router();
 const bodyParser = require("body-parser");
 const path = require("path");
-const { getData } = require("../model/post");
+const { v4: uuidv4 } = require("uuid");
+const { getData, uploadPost, uploadPostInfo } = require("../model/post");
 const multer = require("multer");
 
 const storage = multer.diskStorage({
@@ -17,14 +18,18 @@ const storage = multer.diskStorage({
 const upload = multer({ storage: storage });
 router.use(bodyParser.json());
 router.post("/create-post", upload.array("files"), async (req, res) => {
-  console.log();
+  const { caption, username } = req.body;
   const imgNames = () => {
     return req.files.map((file) => file.filename);
   };
-  console.log(imgNames().toString(), "Img names");
-  const { caption } = req.body;
-  console.log("hit ");
-  console.log(caption);
-  // getData();
+  const imgURL = imgNames().toString();
+  const postID = uuidv4();
+
+  uploadPost(postID, username, "postpictureinfo", "Img post");
+  uploadPostInfo(postID, imgURL, caption, "0");
+  res.status(201).send({
+    message: "Post successfully uploaded",
+    status: "successful",
+  });
 });
 module.exports = router;
