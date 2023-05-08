230031f5f1501f669b6b7657c51d8ae756642372
fzippy <hk2011khan@gmail.com>
2023-05-08 17:34:00 +0100

    Created upload function

--- server/model/post.js
+++ server/model/post.js
@@ -1,6 +1,7 @@
 const { Client } = require("cassandra-driver");
 require("dotenv").config();
 const { v4: uuidv4 } = require("uuid");
+const multer = require("multer");
 const client = new Client({
   cloud: {
     secureConnectBundle: "./secure-connect-digitalgram.zip",
@@ -13,5 +14,14 @@ const client = new Client({
 const getData = async () => {
   const [rows] = await client.execute("SELECT * FROM useractivities.posts");
   console.log(rows);
+};
+
+const uploadPost = () => {
+  // const storage = multer.diskStorage({
+  //   destination: function (req, file, cb) {
+  //     cb(null, "../tmp/uploades");
+  //   },
+  // });
+  // console.log("sss");
 };
 module.exports = { getData };
