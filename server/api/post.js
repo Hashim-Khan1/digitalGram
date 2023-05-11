bbce481100ae91e24f494b39d133909d62d63199
fzippy <hk2011khan@gmail.com>
2023-05-11 22:59:26 +0100

    Change post file location

--- server/api/post.js
+++ server/api/post.js
@@ -8,7 +8,7 @@ const multer = require("multer");
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "uploads");
+    cb(null, "../src/uploads/posts");
   },
   filename: function (req, file, cb) {
     const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
