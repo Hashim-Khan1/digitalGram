196300593bf199631611156454d86ecb612ebc10
fzippy <hk2011khan@gmail.com>
2023-05-30 16:02:35 +0100

    Made onChange search function

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,13 +1,35 @@
+import UserSearchContainer from "./UserSearchContainer";
+import axios from "axios";
+
+const { VITE_API_URL } = import.meta.env;
+
 function Search(props: any) {
+  const handleChange = async (e: any) => {
+    const { value } = e.currentTarget;
+    console.log("ss");
+    if (value == "" || undefined) {
+      console.log("undefined");
+    } else {
+      const instance = axios.create({ baseURL: VITE_API_URL });
+      const res = await instance.post("/user/search", {
+        users: value,
+      });
+      console.log(value, "valyue");
+    }
+  };
   return (
     <>
       <div id="searchContainer">
-        <input
-          type="text"
-          className="searchInpt"
-          placeholder="Search Username"
-        />
-        <div id="search"></div>
+        <div className="row">
+          <input
+            type="text"
+            className="searchInpt"
+            placeholder="Search Username"
+            onChange={handleChange}
+          />
+          <div id="search"></div>
+        </div>
+        <UserSearchContainer username="YolaAboyt" />
       </div>
     </>
   );
