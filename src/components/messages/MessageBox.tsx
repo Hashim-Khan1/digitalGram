d4a7aa766e3ff5c487978a6383f645e5fb6bb690
fzippy <hk2011khan@gmail.com>
2023-08-09 00:05:33 +0100

    Websocket connection from client side

--- src/components/messages/MessageBox.tsx
+++ src/components/messages/MessageBox.tsx
@@ -1,7 +1,49 @@
+import React, { useState, useEffect, useRef, useMemo } from "react";
+
 import FromMessages from "./FromMessages";
 import ToMessages from "./ToMessages";
+import { useLocation } from "react-router-dom";
 
-function MessageBox() {
+function MessageBox(props: any) {
+  const clientID = props.clientID;
+  const [message, setMessage] = useState("");
+  const [ws, setWs] = useState(null);
+  const [inputMessages, setinputMessages] = useState({
+    fromUser: "",
+    toUser: "",
+    friendID: "",
+    userMessage: "",
+  });
+
+  const updateValues = (e) => {
+    const { name, value } = e.currentTarget;
+    setinputMessages((values) => ({
+      ...values,
+      [name]: value,
+    }));
+  };
+  const submitMessage = () => {
+    console.log(inputMessages, "User messages");
+    if (ws && ws.readyState === WebSocket.OPEN) {
+      console.log("messageSent");
+      console.log("messageSent");
+
+      ws.send(JSON.stringify(inputMessages));
+      // ws.send(); // Send the user's message
+      setinputMessages((values) => ({
+        ...values,
+        userMessage: "", // Clear the input field after sending
+      }));
+    }
+  };
+
+  useEffect(() => {
+    const socket = new WebSocket("ws://localhost:3000/Inbox/t/" + clientID);
+    setWs(socket);
+    socket.onopen = () => {
+      console.log("WebSocket Client Connected");
+    };
+  }, [clientID]);
   return (
     <>
       <div id="msgHeader" className="row alignCenter">
@@ -13,7 +55,14 @@ function MessageBox() {
         <ToMessages />
       </div>
       <div className="row">
-        <input type="text" className="inputField" style={{ color: "black" }} />
+        <input
+          type="text"
+          className="inputField"
+          style={{ color: "black" }}
+          name="userMessage"
+          onChange={updateValues}
+          value={inputMessages.userMessage}
+        />
         <div
           style={{
             marginLeft: "auto",
@@ -21,6 +70,7 @@ function MessageBox() {
             padding: "5px 12px",
             cursor: "pointer",
           }}
+          onClick={submitMessage}
         >
           SEND
         </div>
