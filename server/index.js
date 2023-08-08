e18818354329dbd898f4ef3ada74ac2c015dac60
fzippy <hk2011khan@gmail.com>
2023-08-09 00:04:34 +0100

    Parse the websocket data

--- server/index.js
+++ server/index.js
@@ -28,20 +28,23 @@ chat.on("connection", (ws, req, messageID) => {
   ws.id = uuidv4();
   //This is the the room ID
   const room = messageID;
+  //Create the room here
+  if (!rooms[room]) rooms[room] = {};
+  //This will be an empty objecty
+  // console.log(rooms[room]);
+  //Join the room
+  rooms[room] = { ...rooms[room], [ws.id]: ws };
+  //This will be a room with an id, multiple people can have same ID now
+  // console.log(rooms[room]);
   ws.on("message", (message) => {
-    //Create the room here
-    if (!rooms[room]) rooms[room] = {};
-    //This will be an empty objecty
-    // console.log(rooms[room]);
-    //Join the room
-    rooms[room] = { ...rooms[room], [ws.id]: ws };
-    //This will be a room with an id, multiple people can have same ID now
-    // console.log(rooms[room]);
+    const parsedMessage = JSON.parse(message);
+    console.log("Recieved message" + parsedMessage);
+    console.log(parsedMessage);
 
     //Sending the message to the websocket
     Object.values(rooms[room]).forEach((client) => {
       if (client !== ws && client.readyState === WebSocket.OPEN) {
-        const {} = JSON.parse(message);
+        // const {} = JSON.parse(message);
         client.send("Got ur message");
       }
     });
@@ -50,7 +53,7 @@ chat.on("connection", (ws, req, messageID) => {
 
 // Attach the WebSocket upgrade handling to the Express HTTP server manually
 server.on("upgrade", (req, socket, head) => {
-  console.log(req.url);
+  console.log(req.url, "SSSS");
   const urlsplit = req.url.split("/");
   if (urlsplit[1] == "Inbox" && urlsplit.length == 4) {
     chat.handleUpgrade(req, socket, head, (ws) => {
