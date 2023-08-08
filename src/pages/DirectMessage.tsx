98ef5a405064306bfd60bbc4b2ea4ff1e5f6f9c1
fzippy <hk2011khan@gmail.com>
2023-08-09 00:05:12 +0100

    Added params

--- src/pages/DirectMessage.tsx
+++ src/pages/DirectMessage.tsx
@@ -1,21 +1,12 @@
-import { useState, useEffect, useMemo } from "react";
-import { Routes, Route, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
+import MessageBox from "../components/messages/MessageBox";
 
-import axios from "axios";
-import MessageBox from "../components/messages/messageBox";
-
-const { VITE_API_URL } = import.meta.env;
 function DirectMessage() {
   const { ID } = useParams();
-  console.log(ID);
-  const instance = axios.create({
-    baseURL: VITE_API_URL,
-  });
-  useEffect(() => {}, []);
 
   return (
     <>
-      <MessageBox />
+      <MessageBox clientID={ID} />
     </>
   );
 }
